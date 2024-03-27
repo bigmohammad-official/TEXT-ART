2f8b891f1abec473c08eb3a09c4eb92d510c3c95
bigmohammad <141378165+bigmohammad-official@users.noreply.github.com>
2024-03-27 18:37:32 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from art import *
+
+def text_to_art(text):
+    result = text2art(text)
+    print(result)
+
+while True:
+    text = input("Enter a word : ")
+    if text.lower() == '-----_@exit_----': #>>>>="If you type this, the program will close"
+        break
+    text_to_art(text)
